91939d34bd43f52624490b2e80a2f04a19e78042
Vic <syuan0212@gmail.com>
2023-05-20 22:59:16 +0800

    feat: add title update

--- src/components/CardModel.vue
+++ src/components/CardModel.vue
@@ -1,19 +1,9 @@
 <script setup>
 import { ref, computed } from "vue";
-import { useNotification } from "naive-ui";
-import {
-  NButton,
-  NModal,
-  NIcon,
-  NProgress,
-} from "naive-ui";
-import {
-  IosStarOutline,
-  IosStar,
-  MdCalendar,
-  MdTime
-} from "@vicons/ionicons4";
+import { NButton, NModal, NIcon, NProgress, NInput, useNotification } from "naive-ui";
+import { IosStarOutline, IosStar, MdCalendar, MdTime, MdCheckmark } from "@vicons/ionicons4";
 import { ChecklistRound } from "@vicons/material";
+import { Edit20Regular } from "@vicons/fluent";
 import { DateTime } from "luxon";
 import { v4 as uuidv4 } from "uuid";
 import CardDescription from "@/components/cardProperties/CardDescription.vue";
@@ -36,10 +26,11 @@ const segmented = ref({
   footer: "soft",
 });
 const showModal = ref(false);
+const isEditTitle = ref(false);
 const person = ref({
   name: "Iven",
-  avatar: "https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg"
-})
+  avatar: "https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg",
+});
 const cardData = ref({
   title: "卡片線稿圖",
   description: "",
@@ -64,7 +55,7 @@ const cardData = ref({
       avatar: "https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg",
       comment: "留言測試",
       createAt: Date.now(),
-    }
+    },
   ],
   toDoList: [
     {
@@ -72,70 +63,72 @@ const cardData = ref({
       title: "製作 ToDoList",
       workingHours: 0,
       dateRange: [Date.now(), Date.now()],
-      isFinished: false
-    }
-  ]
+      isFinished: false,
+    },
+  ],
 });
 const getCardDescription = (value) => {
   cardData.value.description = value;
-}
+};
 const getCardMembers = (value) => {
   cardData.value.members = value;
-}
+};
 const getCardTags = (value) => {
   cardData.value.tags = value;
-}
+};
 const getCardNotification = (value) => {
   cardData.value.notification = value;
-}
+};
 const getCardDateRange = (value) => {
   cardData.value.dateRange = value;
-}
+};
 const getCardWorkingHours = (value) => {
   cardData.value.workingHours = value;
-}
+};
 const getCardImportance = (value) => {
   cardData.value.importance = value;
-}
+};
 const getCardComment = (value) => {
   cardData.value.comments.push(value);
-}
+};
 const getCardToDoList = (toDoList) => {
   cardData.value.toDoList = toDoList;
-}
+};
 
 const toDoPercent = computed(() => {
-  const filterFinished = cardData.value.toDoList.filter((toDo) => toDo.isFinished === true);
-  let percent = Math.round((filterFinished.length / cardData.value.toDoList.length) * 100) || 0;
+  const filterFinished = cardData.value.toDoList.filter(
+    (toDo) => toDo.isFinished === true
+  );
+  let percent =
+    Math.round((filterFinished.length / cardData.value.toDoList.length) * 100) || 0;
   return percent;
-})
+});
 const toDoListWorkingHoursCount = computed(() => {
   let count = 0;
   cardData.value.toDoList.forEach((toDo) => {
     count += toDo.workingHours;
   });
   return count;
-})
+});
 const daysDiff = (startTimeStamp, endTimeStamp) => {
   const startDate = DateTime.fromMillis(startTimeStamp);
   const endDate = DateTime.fromMillis(endTimeStamp);
-  const diff = endDate.diff(startDate, 'days').toObject();
+  const diff = endDate.diff(startDate, "days").toObject();
   const daysDiff = Math.ceil(diff.days);
   return daysDiff;
-}
+};
 const submitCardData = () => {
   notification.create({
     title: "送出卡片",
     content: `已儲存`,
     duration: 2000,
     closable: false,
-    meta: DateTime.fromMillis(Date.now()).toFormat('yyyy/MM/dd'),
+    meta: DateTime.fromMillis(Date.now()).toFormat("yyyy/MM/dd"),
     onAfterLeave: () => {
       showModal.value = false;
-    }
+    },
   });
-
-}
+};
 </script>
 
 <template>
@@ -154,9 +147,36 @@ const submitCardData = () => {
   >
     <!-- Card title -->
     <div class="flex justify-between mb-9">
-      <h3 class="text-2xl">{{ cardData.title }}</h3>
+      <h3 class="flex items-center text-2xl group">
+        <template v-if="!isEditTitle">
+          {{ cardData.title }}
+        </template>
+        <template v-else>
+          <n-input
+            size="small"
+            @keydown.enter="isEditTitle = false"
+            v-model:value="cardData.title"
+            placeholder="請輸入卡片標題"
+            class="mr-3"
+          />
+        </template>
+        <span>
+          <n-icon
+            v-if="!isEditTitle"
+            size="20"
+            :component="Edit20Regular"
+            class="hidden text-gray-400 cursor-pointer group-hover:block hover:text-gray-800 ml-1"
+            @click="isEditTitle = true"
+          />
+          <n-icon v-else size="20" :component="MdCheckmark" class="text-gray-400 cursor-pointer hover:text-gray-800" @click="isEditTitle = false"/>
+        </span>
+      </h3>
       <button @click="cardData.isPinned = !cardData.isPinned">
-        <n-icon size="20" :component="cardData.isPinned ? IosStar : IosStarOutline" :class="cardData.isPinned? 'text-red-500': ''"/>
+        <n-icon
+          size="20"
+          :component="cardData.isPinned ? IosStar : IosStarOutline"
+          :class="cardData.isPinned ? 'text-red-500' : ''"
+        />
       </button>
     </div>
     <!-- Card header Info -->
@@ -164,9 +184,21 @@ const submitCardData = () => {
       <li class="flex">
         <n-icon size="40" :component="MdCalendar" class="text-red-500 mr-5" />
         <div>
-          <p class="text-xl">{{ Date.now() < cardData.dateRange[0]? `還有 ${ daysDiff(Date.now(), cardData.dateRange[0]) } 天開始` : `倒數 ${ daysDiff(cardData.dateRange[0], cardData.dateRange[1]) } 天` }}</p>
-          <p class="text-sm">於 {{ DateTime.fromMillis(cardData.dateRange[0]).toFormat('yyyy/MM/dd') }} 開始</p>
-          <p class="text-sm">於 {{ DateTime.fromMillis(cardData.dateRange[1]).toFormat('yyyy/MM/dd') }} 到期</p>
+          <p class="text-xl">
+            {{
+              Date.now() < cardData.dateRange[0]
+                ? `還有 ${daysDiff(Date.now(), cardData.dateRange[0])} 天開始`
+                : `倒數 ${daysDiff(cardData.dateRange[0], cardData.dateRange[1])} 天`
+            }}
+          </p>
+          <p class="text-sm">
+            於
+            {{ DateTime.fromMillis(cardData.dateRange[0]).toFormat("yyyy/MM/dd") }} 開始
+          </p>
+          <p class="text-sm">
+            於
+            {{ DateTime.fromMillis(cardData.dateRange[1]).toFormat("yyyy/MM/dd") }} 到期
+          </p>
         </div>
       </li>
       <li class="flex">
@@ -177,7 +209,8 @@ const submitCardData = () => {
             >{{ cardData.workingHours }} 小時
           </p>
           <p class="text-xl">
-            <span class="text-base text-grey-500 mr-2">已使用工時</span>{{ toDoListWorkingHoursCount }} 小時
+            <span class="text-base text-grey-500 mr-2">已使用工時</span
+            >{{ toDoListWorkingHoursCount }} 小時
           </p>
         </div>
       </li>
@@ -199,7 +232,10 @@ const submitCardData = () => {
       <ul>
         <!-- Card Description -->
         <li class="mb-5">
-          <CardDescription :description="cardData.description" @update="getCardDescription" />
+          <CardDescription
+            :description="cardData.description"
+            @update="getCardDescription"
+          />
         </li>
         <!-- Card Members -->
         <li class="mb-5">
@@ -211,7 +247,10 @@ const submitCardData = () => {
         </li>
         <!-- Card Notify -->
         <li class="mb-5">
-          <CardNotifications :notification="cardData.notification" @update="getCardNotification" />
+          <CardNotifications
+            :notification="cardData.notification"
+            @update="getCardNotification"
+          />
         </li>
         <!-- Card DeadLine -->
         <li class="mb-5">
@@ -219,7 +258,10 @@ const submitCardData = () => {
         </li>
         <!-- Card WorkingHours -->
         <li class="mb-5">
-          <CardWorkingHours :workingHours="cardData.workingHours" @update="getCardWorkingHours" />
+          <CardWorkingHours
+            :workingHours="cardData.workingHours"
+            @update="getCardWorkingHours"
+          />
         </li>
         <!-- Card Importance -->
         <li class="mb-5">
@@ -230,7 +272,11 @@ const submitCardData = () => {
     <!-- Card Comment -->
     <section class="pb-6 mb-6 border-b">
       <h4 class="text-2xl mb-9">評論</h4>
-      <CardComments :person="person" :comments="cardData.comments" @updateComment="getCardComment"/>
+      <CardComments
+        :person="person"
+        :comments="cardData.comments"
+        @updateComment="getCardComment"
+      />
     </section>
     <!-- Card Content -->
     <section class="pb-6 mb-6">
@@ -240,7 +286,7 @@ const submitCardData = () => {
     </section>
     <section class="pb-6 mb-6">
       <h4 class="text-2xl">待辦清單</h4>
-      <CardToDoList :toDoList="cardData.toDoList" @updateToDoList="getCardToDoList"/>
+      <CardToDoList :toDoList="cardData.toDoList" @updateToDoList="getCardToDoList" />
     </section>
     <template #footer>
       <div class="flex justify-around">
